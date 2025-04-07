d099e65b2581966a44e28045a9d42bfe75e77992
susons <kristapsbulssusons@gmail.com>
2025-04-08 01:24:42 +0300

    [impr] Add description

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,16 +1,22 @@
 "use server";
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { convertToObj } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 export const getLatestProducts = async () => {
-  const prisma = new PrismaClient();
-
   const products = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
   return convertToObj(products);
+};
+
+// Get single product by slug
+
+export const getProductBySlug = async (slug: string) => {
+  return await prisma.product.findFirst({
+    where: { slug },
+  });
 };
